61c34947dea38a53ff837ac21358980d88d6cf0c
juleni <69912179+juleni@users.noreply.github.com>
2023-09-06 14:22:39 +0200

    Moved to components folder

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
     <header>
       <div>
         <Link href="/">
-          <h1>JULENIs MD Blog</h1>
+          <h1>JULENI&apos;s MD Blog</h1>
         </Link>
         <br />
       </div>
