2e6f2666bda98d87ba5a0515633fd798d97b25ad
juleni <69912179+juleni@users.noreply.github.com>
2023-09-06 17:39:41 +0200

    Added logo and styles

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,15 +1,29 @@
 import Link from "@/node_modules/next/link";
+import Image from "next/image";
 
 function Header() {
   return (
     <header>
       <div className="text-center bg-slate-800 p-2 my-6 rounded-md">
-        <Link href="/">
-          <h1 className="text-3xl text-green-600 mt-2 font-medium">
-            My MarkDown Blog
-          </h1>
-        </Link>
-        <p className="text-slate-300 m-2">🔰 Welcome to my personal blog 🔰</p>
+        <div className="">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className="mx-auto"
+          />
+          <div className="">
+            <Link href="/">
+              <h1 className="text-3xl text-green-600 mt-2 font-medium">
+                My MarkDown Blog
+              </h1>
+            </Link>
+            <p className="text-slate-300 m-2">
+              🔰 Welcome to my personal blog 🔰
+            </p>
+          </div>
+        </div>
       </div>
     </header>
   );
