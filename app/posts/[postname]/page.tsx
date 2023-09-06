2324d058f0594b0ddcc284a57752ace7831539f8
juleni <69912179+juleni@users.noreply.github.com>
2023-09-06 12:04:53 +0200

    Added getPostContent() and Markdown

--- app/posts/[postname]/page.tsx
+++ app/posts/[postname]/page.tsx
@@ -1,8 +1,21 @@
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+
+const getPostContent = (postName: string) => {
+  const folder = "posts/";
+  const file = `${folder}/${postName}.md`;
+  const content = fs.readFileSync(file, "utf8");
+
+  return content;
+};
+
 export default function postPage(props: any) {
   const postName = props.params.postname;
+  const postContent = getPostContent(postName);
   return (
-    <p>
+    <div>
       <h1>Post {postName}</h1>
-    </p>
+      <Markdown>{postContent}</Markdown>
+    </div>
   );
 }
