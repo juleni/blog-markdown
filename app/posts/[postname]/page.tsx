249096396c6b73bd66b8470f7008c5c13a217350
juleni <69912179+juleni@users.noreply.github.com>
2023-09-06 14:24:10 +0200

    Added gray-matter

--- app/posts/[postname]/page.tsx
+++ app/posts/[postname]/page.tsx
@@ -1,21 +1,23 @@
 import fs from "fs";
+import matter from "gray-matter";
 import Markdown from "markdown-to-jsx";
 
 const getPostContent = (postName: string) => {
   const folder = "posts/";
   const file = `${folder}/${postName}.md`;
   const content = fs.readFileSync(file, "utf8");
+  const matterResult = matter(content);
 
-  return content;
+  return matterResult;
 };
 
 export default function postPage(props: any) {
   const postName = props.params.postname;
-  const postContent = getPostContent(postName);
+  const post = getPostContent(postName);
   return (
     <div>
-      <h1>Post {postName}</h1>
-      <Markdown>{postContent}</Markdown>
+      <h1>Post {post.data.title}</h1>
+      <Markdown>{post.content}</Markdown>
     </div>
   );
 }
