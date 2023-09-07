5f349eff59435833c478a9db10aa2a8d04bc6f51
juleni <69912179+juleni@users.noreply.github.com>
2023-09-07 09:47:38 +0200

    Fixed styles - for more responsible

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer>
-      <div className="text-center border-t border-slate-300 py-6 mt-6 text-slate-400">
+      <div className="text-xs sm:text-sm text-center border-t border-slate-300 py-6 mt-6 text-slate-400">
         2023 Created by JULENI
       </div>
     </footer>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,22 +4,25 @@ import Image from "next/image";
 function Header() {
   return (
     <header>
-      <div className="text-center bg-slate-800 p-2 my-6 rounded-md">
-        <div className="">
-          <Image
-            src="/logo.png"
-            alt="Logo"
-            width={100}
-            height={100}
-            className="mx-auto"
-          />
+      <div className="text-center bg-slate-700 p-2 my-6 rounded-md">
+        <div className="flex flex-no-wrap justify-center items-center">
           <div className="">
+            <Image
+              src="/logo.png"
+              alt="Logo"
+              width={70}
+              height={70}
+              className="mx-auto sm:w-[100px]"
+            />
+          </div>
+
+          <div className="pl-2 sm:pl-6">
             <Link href="/">
-              <h1 className="text-3xl text-green-600 mt-2 font-medium">
+              <h1 className="text-xl sm:text-3xl text-green-600 mt-2 font-medium">
                 My MarkDown Blog
               </h1>
             </Link>
-            <p className="text-slate-300 m-2">
+            <p className="text-xs sm:text-lg text-slate-300 m-2">
               🔰 Welcome to my personal blog 🔰
             </p>
           </div>
