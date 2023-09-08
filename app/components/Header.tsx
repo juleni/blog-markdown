33390d92cffb31738925c989c44ec115eee55764
juleni <69912179+juleni@users.noreply.github.com>
2023-09-08 09:45:58 +0200

    Added link to image and particular Tooltip

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "@/node_modules/next/link";
 import Image from "next/image";
+import { Tooltip } from "react-tooltip";
 
 function Header() {
   return (
@@ -7,13 +10,22 @@ function Header() {
       <div className="text-center bg-slate-700 p-2 my-6 rounded-md">
         <div className="flex flex-no-wrap justify-center items-center">
           <div className="">
-            <Image
-              src="/logo.png"
-              alt="Logo"
-              width={70}
-              height={70}
-              className="mx-auto sm:w-[100px]"
-            />
+            <a
+              target="_blank"
+              href="https://dillinger.io/"
+              rel="noopener"
+              data-tooltip-id="img-tooltip"
+              data-tooltip-content="Run Dillinger to edit markdown file online"
+            >
+              <Image
+                src="/logo.png"
+                alt="Run Dillinger to edit markdown file online"
+                width={70}
+                height={70}
+                className="mx-auto sm:w-[100px]"
+              />
+            </a>
+            <Tooltip id="img-tooltip" />
           </div>
 
           <div className="pl-2 sm:pl-6">
